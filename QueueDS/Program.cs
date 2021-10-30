a4dc32641d4061024b279fc31a4b6bdb17b0b490
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-30 20:06:15 +0530

    [shweta] added the hello message

---
+++ QueueDS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
